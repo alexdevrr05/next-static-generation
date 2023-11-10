ea1ebc3d9bcd32c4bd3ddf94e26de08e86a17fd8
alexdevrr05 <pv190115553@vallarta.tecmm.edu.mx>
2023-11-10 17:17:16 -0600

    Refactor Pokemon list display

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,52 @@
 import { GetStaticProps, NextPage } from 'next';
+import axios from 'axios';
+
 import { Layout } from '../components/layouts';
-
 import { pokeApi } from '@/api';
+import { PokemonListResponse, SmallPokemon } from '@/interfaces';
 
-const Home: NextPage = (props) => {
-  console.log(props);
+interface Props {
+  pokemons: SmallPokemon[];
+}
+
+const Home: NextPage<Props> = ({ pokemons }) => {
   return (
     <Layout title='Pokemon List'>
       <ul>
-        <li>Pokemon</li>
-        <li>Pokemon</li>
-        <li>Pokemon</li>
-        <li>Pokemon</li>
-        <li>Pokemon</li>
-        <li>Pokemon</li>
+        {pokemons.map(({ name, id }) => (
+          <li key={id} className='font-semibold'>
+            {name}
+          </li>
+        ))}
       </ul>
     </Layout>
   );
 };
 
 export const getStaticProps: GetStaticProps = async (ctx) => {
-  const { data } = await pokeApi.get('/pokemon?limit=151');
+  const { data } = await pokeApi.get<PokemonListResponse>('/pokemon?limit=151');
+  const pokemons: SmallPokemon[] = data.results.map((pokemon, i) => ({
+    ...pokemon,
+    id: i + 1,
+    img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${
+      i + 1
+    }.svg`,
+  }));
+
+  // const pokemonsUrls = pokemonsArray.map((pokemon) => axios.get(pokemon.url));
+  // const pokemonsUrlResp = await Promise.all(pokemonsUrls);
+  // const pokemons: SmallPokemon[] = pokemonsUrlResp.map(({ data }) => {
+  //   return {
+  //     id: data.id,
+  //     name: data.name,
+  //     url: `https://pokeapi.co/api/v2/pokemon/${data.id}`,
+  //     img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${data.id}.svg`,
+  //   };
+  // });
 
   return {
     props: {
-      pokemons: data.results,
+      pokemons,
     },
   };
 };
