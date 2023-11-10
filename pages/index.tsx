d5b296a1e8ed0797f4a545b26c15e497bea2722a
alexdevrr05 <pv190115553@vallarta.tecmm.edu.mx>
2023-11-10 10:16:09 -0600

    Add NextUIProvider, update config styles in tailwind.config.js

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,21 @@
-import Head from 'next/head'
+import { Button } from '@nextui-org/react';
+import Head from 'next/head';
 
 export default function Home() {
   return (
     <>
       <Head>
         <title>Create Next App</title>
-        <meta name="description" content="Pokemon static" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <meta name='description' content='Pokemon static' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
-      <main>
-        <h1 className='text-3xl font-bold underline'>Hello world</h1>
+      <main className='mx-2 my-2'>
+        <h1 className='text-3xl font-bold mb-2'>Hello world</h1>
+        <Button color='primary'>
+          <h1>Button</h1>
+        </Button>
       </main>
     </>
-  )
+  );
 }
