00ade20e91601a5ce2d8637d553f489223dd5cc4
alexdevrr05 <pv190115553@vallarta.tecmm.edu.mx>
2023-11-11 09:59:05 -0600

    Add new Pokemon interface and update Pokemon page with data fetching.

--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -1,15 +1,52 @@
-import { Layout } from '@/components/layouts';
+import { GetStaticPaths, GetStaticProps, NextPage } from 'next';
 import { useRouter } from 'next/router';
 
-const PokemonPage = () => {
-  const router = useRouter();
-  console.log(router.query);
+import { Layout } from '@/components/layouts';
+import { pokeApi } from '@/api';
+import { Pokemon } from '@/interfaces';
 
+interface Props {
+  pokemon: Pokemon;
+}
+
+const PokemonPage: NextPage<Props> = ({ pokemon }) => {
+  const router = useRouter();
   return (
     <Layout>
-      <h1 className='font-bold'>PokemonPage</h1>
+      <h1 className='font-bold'>{pokemon.name}</h1>
     </Layout>
   );
+};
+
+// Esto solo se define porque la ruta es dinámica ([id.tsx])
+export const getStaticPaths: GetStaticPaths = () => {
+  const pokemons151 = [...Array(151)].map((value, index) => `${index + 1}`);
+
+  return {
+    // paths: [{ params: { id: '1' } }],
+    paths: pokemons151.map((id) => ({
+      params: { id },
+    })),
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  const { id } = ctx.params as { id: string };
+
+  const { data } = await pokeApi.get<Pokemon>(`/pokemon/${id}`);
+
+  const pokemon = {
+    name: data.forms[0].name,
+    stats: data.stats,
+    moves: data.moves,
+  };
+
+  return {
+    props: {
+      pokemon,
+    },
+  };
 };
 
 export default PokemonPage;
